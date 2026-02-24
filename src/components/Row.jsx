1452071a04bd91b6fb5acc1226d0813cca279d3d
Talent <talentpthomas@gmail.com>
2026-02-24 18:24:17 +0530

    sliding arrow added not working thou

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
 const Row = ({ title, requestURL }) => {
   const [movies, setMovies] = useState([]);
 
@@ -16,7 +18,11 @@ const Row = ({ title, requestURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          size={35}
+          className="bg-white rounded-full opacity-50 absolute z-10 hidden group-hover:block"
+        />
         <div
           id={"slider"}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
@@ -25,6 +31,10 @@ const Row = ({ title, requestURL }) => {
             return <Movie movie={movie} />;
           })}
         </div>
+        <MdChevronRight
+          size={35}
+          className="bg-white rounded-full opacity-50 absolute end-0 hidden group-hover:block"
+        />
       </div>
     </>
   );
